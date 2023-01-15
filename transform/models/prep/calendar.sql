1fd78f92b968f23721f33e8b18458dda7e4b3c15
bbuxton93 <you@example.com>
2023-01-15 13:52:00 -0600

    add ff

--- transform/models/prep/calendar.sql
+++ transform/models/prep/calendar.sql
@@ -1,3 +1,8 @@
+
+{% set load_tables = ["activities", "draft", "free_agent_box","matchup_results","player_box","player_season","results","teams"] %}
+
+
+
 SELECT
     *
 FROM  {{ source( 'cfb_prep', 'calendar' ) }}
